213357259aff9c7a690b86d91f45666fe1aa42e3
ARTMARVL <vladyslav.anishchenko@gmail.com>
2026-06-07 20:00:17 +0200

    Task6

---
+++ src/Equipment.java
@@ -0,0 +1,25 @@
+public abstract class Equipment implements Displayable {
+    private String id;
+    private String name;
+    private double baseDailyPrice;
+    private boolean available;
+    public Equipment(String id, String name, double baseDailyPrice) {
+        this.id = id;
+        this.name = name;
+        this.baseDailyPrice = baseDailyPrice;
+        this.available = true;
+    }
+
+    public String getId() { return id; }
+    public String getName() { return name; }
+    public double getBaseDailyPrice() { return baseDailyPrice; }
+    public boolean isAvailable() { return available; }
+    public void setAvailable(boolean available) { this.available = available; }
+    public abstract double calculateDailyPrice();
+
+    @Override
+    public String getDisplayText() {
+        return String.format("[%s] %s | Базовая цена: %.2f | Доступен: %s",
+                id, name, calculateDailyPrice(), available ? "Да" : "Нет");
+    }
+}
